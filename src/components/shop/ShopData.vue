b6874f6612c7af67d04c9e7f2a05e75155627712
S1mple <Z15225825811@163.com>
2021-01-16 15:25:49 +0800

    前端新增

--- src/components/shop/ShopData.vue
+++ src/components/shop/ShopData.vue
@@ -48,6 +48,8 @@
         </el-table-column>
 
       </el-table>
+
+      <el-button type="success" @click="toadd">新增</el-button>
       <!--分页-->
       <el-pagination
         align="right"
@@ -59,6 +61,63 @@
         :total="count"
       >
       </el-pagination>
+
+
+
+
+
+      <!--新增模板-->
+      <el-dialog title="品牌信息" :visible.sync="addFormFlag">
+
+        <el-form :model="addForm" ref="addForm"   label-width="100px">
+
+          <el-form-item label="属性英文名" prop="name">
+            <el-input v-model="addForm.name" autocomplete="off" ></el-input>
+          </el-form-item>
+
+          <el-form-item label="属性中文名" prop="nameCH">
+            <el-input v-model="addForm.nameCH" autocomplete="off" ></el-input>
+          </el-form-item>
+
+
+
+          <el-form-item label="商品类型" prop="typeId">
+          <el-select v-model="addForm.typeId" placeholder="请选择">
+            <el-option
+              v-for="item in TypeDatas"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id">
+            </el-option>
+          </el-select>
+          </el-form-item>
+
+          <el-form-item label="属性类型" prop="type">
+            <el-radio v-model="addForm.type" label="0">下拉框</el-radio>
+            <el-radio v-model="addForm.type" label="1">单选框</el-radio>
+            <el-radio v-model="addForm.type" label="2">复选框</el-radio>
+            <el-radio v-model="addForm.type" label="3">输入框</el-radio>
+          </el-form-item>
+
+          <el-form-item label="是否为SKU" prop="isSKU">
+            <el-radio v-model="addForm.isSKU" label="0">是</el-radio>
+            <el-radio v-model="addForm.isSKU" label="1">否</el-radio>
+          </el-form-item>
+
+
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="addFormFlag = false">取 消</el-button>
+          <el-button type="primary" @click="addShopData">确 定</el-button>
+        </div>
+      </el-dialog>
+
+
+
+
+
+
+
     </div>
 </template>
 
@@ -68,12 +127,31 @@
         data(){
           return{
             ShopData:[],
+            TypeDatas:[],
             TypeData:[],
             count:0,
             sizes:[2,3,5,10],
             size:2,
+            addForm:{
+              id:"",
+              name:"",
+              nameCH:"",
+              typeId:"",
+              type:"",
+              isSKU:"",
+            },
+            addFormFlag:false,
           }
         },methods:{
+        toadd:function(){
+            this.addFormFlag=true
+        },addShopData:function(){
+          console.log(this.addForm)
+          this.$axios.post("http://localhost:8080/api/shopData/add",this.$qs.stringify(this.addForm)).then(res=>{
+            this.addFormFlag=false
+            this.queryData(1)
+          }).catch(err=>console.log(err))
+        },
         handleCurrentChange:function(page){ //跳转页面
           console.log(page);
           this.queryData(page);
@@ -97,13 +175,32 @@
           this.$axios.get("http://localhost:8080/api/type/getData").then(res=>{
             //console.log(res)
             this.TypeData=res.data.data;
+            this.getTypeDatas()
           }).catch(err=>console.log(err))
         },changetype:function (row, column) {
           return row.type==0?"下拉框":row.type==1?"单选框":row.type==2?"复选框":"输入框"
+        },getTypeDatas(){
+          debugger;
+          for (var i = 0; i <this.TypeData.length ; i++) {
+            var rs  =this.isParent(this.TypeData[i])
+            if (rs==false){
+              this.TypeDatas.push(this.TypeData[i])
+              //console.log(this.TypeDatas)
+            }
+          }
+        },isParent:function (datas) {
+          for (var j = 0; j <this.TypeData.length ; j++) {
+            if (datas.id==this.TypeData[j].pid){
+             return true
+            }
+          }
+          return false
         }
       },created:function () {
         this.queryData(1)
         this.getTypeData()
+
+
       }
     }
 </script>
